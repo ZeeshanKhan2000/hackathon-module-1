530516dfc64069bd6d95726b96bfefa0ee6f2d7a
ZeeshanKhan2000 <107643990+ZeeshanKhan2000@users.noreply.github.com>
2022-10-31 11:40:41 -0700

    Add files via upload

---
+++ project-4/assets/js/custom.js
@@ -0,0 +1,33 @@
+$('.banner-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    dots: false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+// Fancybox Config
+$('[data-fancybox="gallery"]').fancybox({
+    buttons: [
+      "slideShow",
+      "thumbs",
+      "zoom",
+      "fullScreen",
+      "share",
+      "close"
+    ],
+    loop: false,
+    protect: true
+  });
+  
